db641ec9c3923413692bd3c80248779304f15edc
lemon <lemon@gmail.com>
2022-02-21 21:15:28 +0800

    404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,13 +2,24 @@
  * @Author: 大侠传授两招吧
  * @Date: 2022-02-21 20:07:22
  * @LastEditors: 大侠传授两招吧
- * @LastEditTime: 2022-02-21 20:07:23
+ * @LastEditTime: 2022-02-21 21:02:38
  * @Description: 
 -->
 <template>
     <div class="notfound">
-        <h1>404</h1>
-   </div>
+        <img src="https://wpimg.wallstcn.com/0e03b7da-db9e-4819-ba10-9016ddfdaed3" />
+
+        <div class="notfound-right">
+            <div class="tips">
+                <h1>哎呀！</h1>
+                <div>版权所有<span>隔壁老王</span></div>
+                <h3>不能进入这个页面...</h3>
+                <h4>请检查您输入的网址是否正确，或点击下方按钮返回首页。</h4>
+            </div>
+
+            <el-button class="back-home" type="primary" round @click="$router.replace('/home')">返回首页</el-button>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -16,7 +27,55 @@
 </script>
 
 <style lang="scss" scoped>
-.notfound{
+.notfound {
+    width: 100%;
+    height: 100%;
+    background-color: #e3e3e3;
+    @include flexCenter();
 
+    .back-home {
+        min-width: 110px;
+        margin-top: 30px;
+        a {
+            color: #fff;
+        }
+    }
+    img {
+        width: 555px;
+        margin-left: -200px;
+    }
+    &-right {
+        margin-top: 100px;
+        margin-left: -100px;
+        .tips {
+            width: 300px;
+            margin: auto;
+            text-align: left;
+        }
+        h1 {
+            font-size: 32px;
+            line-height: 40px;
+            color: #1482f0;
+        }
+        h3 {
+            color: #222;
+            font-size: 20px;
+            font-weight: 600;
+        }
+        h4 {
+            margin-top: 20px;
+            color: grey;
+            font-size: 13px;
+            line-height: 22px;
+        }
+        div {
+            margin: 24px 0;
+            font-size: 12px;
+        }
+        span {
+            margin-left: 4px;
+            color: rgb(32, 160, 255);
+        }
+    }
 }
 </style>
